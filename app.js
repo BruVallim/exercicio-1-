4eb85f7e05441694aa305553766c31337ab41068
BVallim <brunavallimdebem19@gmail.com>
2024-05-25 02:46:46 -0300

    exercicio 1

---
+++ app.js
@@ -0,0 +1,8 @@
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'sabado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
